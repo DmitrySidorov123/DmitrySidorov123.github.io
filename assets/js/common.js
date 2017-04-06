b7c82e8405bfddfe25f9132eb8f1b4b5c20ed380
DmitryPolimushket <sidsid007@gmail.com>
2017-04-06 15:43:49 +0300

    initial commit

---
+++ assets/js/common.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+
+    $('.country').styler();
+
+    $('form').submit(function () {
+            this.ajax({
+                url: 'ajax/test.html',
+                success: function(){
+                    alert('success!');
+                }
+            });
+        });
+});
